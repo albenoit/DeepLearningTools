merged some options, inclding mixed precision and various loss function fixes and added some attention functions

experiments_manager.py
@@ -108,6 +108,9 @@ try:
     import tensorflow_addons as tfa
 except:
     print('WARNING, tensorflow_addons could not be loaded, this may generate errors but should not impact model serving')
+
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
 #constants
 SETTINGSFILE_COPY_NAME='experiment_settings.py'
 WEIGHTS_MOVING_AVERAGE_DECAY=0.998
@@ -162,7 +165,6 @@ class MyCustomModelSaverExporterCallback(tf.keras.callbacks.ModelCheckpoint):
                                                               )
     self.settings=settings
     self.settings.model_export_filename=settings.sessionFolder+'/exported_models'
-    self.version_count=0
 
   def on_epoch_end(self, epoch, logs=None):
     #call parent function
@@ -188,16 +190,62 @@ class MyCustomModelSaverExporterCallback(tf.keras.callbacks.ModelCheckpoint):
     if current==self.best:
       print('EXPORTING A NEW MODEL VERSION FOR SERVING')
       print('Exporting model at epoch {}.'.format(epoch))
-      self.version_count+=1
       exported_module=usersettings.get_served_module(self.model, self.settings.model_name)
       if not(hasattr(exported_module, 'served_model')):
         raise ValueError('Experiment settings file MUST have \'served_model\' function with @tf.function decoration.')
+      output_path='{folder}/{version}'.format(folder=self.settings.model_export_filename, version=epoch)
+      signatures={self.settings.model_name:exported_module.served_model}
       tf.saved_model.save(
         exported_module,
-        '{folder}/{version}'.format(folder=self.settings.model_export_filename, version=self.version_count),
-        signatures={self.settings.model_name:exported_module.served_model},
+        output_path,
+        signatures=signatures,
         options=None
         )
+      '''export to TensorRT, WIP
+       refer to https://www.tensorflow.org/api_docs/python/tf/experimental/tensorrt/Converter
+       and
+       https://docs.nvidia.com/deeplearning/frameworks/tf-trt-user-guide/index.html
+      '''
+      try:
+        '''params=None
+        if True:#precision_mode is not None:
+          precision_mode='FP16'
+        #tensorflow doc/issue ConversionParams not found
+        params = tf.experimental.tensorrt.ConversionParams(
+                          precision_mode=precision_mode,
+        # Set this to a large enough number so it can cache all the engines.
+        maximum_cached_engines=16)
+        converter = tf.experimental.tensorrt.Converter(
+              input_saved_model_dir=output_path,
+              input_saved_model_signature_key=signatures,
+              conversion_params=params)
+    
+      
+        
+        #NVIDIA doc: 
+        from tensorflow.python.compiler.tensorrt import trt_convert as trt
+        conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS
+        print('conversion_params',conversion_params)
+        conversion_params = conversion_params._replace(
+        max_workspace_size_bytes=(1<<32))
+        conversion_params = conversion_params._replace(precision_mode="FP16")
+        conversion_params = conversion_params._replace(maximum_cached_engines=100)
+
+        print('creating converter')
+        converter = tf.experimental.tensorrt.Converter(#trt.TrtGraphConverterV2(
+          input_saved_model_dir=output_path,
+          input_saved_model_signature_key=signatures,
+          conversion_params=conversion_params)
+
+        print('converting')
+        converter.convert()
+        print('saving')
+        converter.save(output_path+'_trt')
+        print('Exported model to NVIDIA TensorRT')
+        '''
+      except Exception as e:
+        print('Failed to export to TensorRT but original saved model has been exported. Reported error:',e)
+
       print('Model export OK at epoch {}.'.format(epoch))
       older_versions=os.listdir(self.settings.model_export_filename)
       print('Available model versions:',older_versions)
@@ -330,6 +378,14 @@ def run_experiment(usersettings):
       if usersettings.weights_moving_averages:
         optimizer=tfa.optimizers.MovingAverage(optimizer, average_decay=0.9999, name='weights_ema')
       metrics=usersettings.get_metrics(model, loss)
+      
+      if usersettings.enable_mixed_precision:
+        # use AMP
+        print('Using Automatic Mixed Precision along the optimization process')
+        policy = mixed_precision.Policy('mixed_float16')
+        mixed_precision.set_policy(policy)
+        #optimizer = tf.train.experimental.enable_mixed_precision_graph_rewrite(optimizer)
+
       print('Compiling model...')
       model.compile(optimizer=optimizer,
                     loss=loss,# you can use a different loss on each output by passing a dictionary or a list of losses
@@ -348,9 +404,16 @@ def run_experiment(usersettings):
   #-> as an image in the session folder
   model_name_str=usersettings.model_name
   try:
-    plot_model(model, to_file=usersettings.sessionFolder+'/'+model_name_str+'.png')
+    from tensorflow.keras.layers import Layer
+    model._layers = [
+        layer for layer in model._layers if isinstance(layer, Layer)
+    ]
+    plot_model(model,
+               to_file=usersettings.sessionFolder+'/'+model_name_str+'.png',
+               show_shapes=True)
+    input('Graph drawn, written here', usersettings.sessionFolder+'/'+model_name_str+'.png')
   except Exception as e:
-    print('Counld not plot model, error:',e)
+    print('Could not plot model, error:',e)
 
   #-> as a printed log and write the network summary to file in the session folder
   with open(usersettings.sessionFolder+'/'+model_name_str+'.txt','w') as fh:

helpers/loss.py
@@ -1,5 +1,9 @@
 ''' helpers_loss, a collection of helpers to compute various losses and related tools
     @author, Alexandre Benoit, LISTIC Lab, FRANCE
+
+    WARNING, many loss to be tested ! check/compare with the original papers !!!
+
+    NOTE,maybe have a look here: https://niftynet.readthedocs.io/en/dev/niftynet.layer.loss_segmentation.html
 '''
 import tensorflow as tf
 import numpy as np
@@ -82,7 +86,49 @@ def preds_labels_preprocess_softmax_flatten(logits, labels):
   y_true, y_pred=get_batch_flat_tensors(labels, pred_probs)
   return y_true, y_pred
 
+@tf.function
+def smooth_labels(labels, factor):
+    # smooth the labels
+    labels *= (tf.constant(1., tf.float32) - factor)
+    labels += (factor / labels.shape[-1])
+    #tf.print('labels', labels)
+    return labels
 
+def weighted_xcrosspow_loss_softmax(logits, labels, gamma=0.3, weight_class_sample_prob=False, weight_class_global_prob=False, train_class_probs=None, name='loss'):
+    """
+    a cross entropy loss with a power low, same idea as for focal loss but, limits oversupression of high scores
+    from https://arxiv.org/pdf/1809.00076.pdf
+    added specific weightings
+
+    Args:
+      labels: A tensor of shape [batch_size,...] with class indexes (that willbe one hot encoded internally).
+      logits: A float32 tensor of shape [batch_size,...,num_classes].
+      gamma
+      weight_class_sample_prob: if True, per sample class loss by the related class sample probability
+      weight_class_global_prob, set True to weight the loss with respect to train dataset true class probabilities
+      train_class_probs, a numpy array of class weights
+    Returns:
+      A cross entropy tensor of shape [batchsize, classes]
+    """
+    y_true, y_pred=preds_labels_preprocess_softmax_flatten(logits, labels)
+    
+    eps=1e-10
+    y_pred=tf.clip_by_value(y_pred, eps, 1.-eps)#avoid undefined values
+    #standard cross entropy ^ gamma
+    L=y_true*tf.math.pow(-tf.math.log(y_pred), gamma)
+
+    if weight_class_sample_prob:
+        class_rates = get_sample_class_probabilities(y_true)
+        class_rates=tf.reshape(class_rates, [-1,1,y_pred.shape[-1]])
+        weighting_factor=pow(tf.ones_like(L) * 3.0, class_rates)
+        L*=weighting_factor
+    if weight_class_global_prob:
+      train_class_weights_factor=tf.constant(train_class_probs, dtype=tf.float32)
+      train_class_weights_factor=tf.reshape(train_class_weights_factor, [-1,1,y_pred.shape[-1]])
+      L*=train_class_weights_factor
+      per_sample_loss=tf.reduce_sum(L, axis=-1)
+      return tf.math.reduce_mean(per_sample_loss, name=name)
+            
 def focal_loss_softmax(logits, labels,  gamma=3., weight_class_sample_prob=False, weight_class_global_prob=False, train_class_probs=None, name='loss'):
     """
     Focal loss, a cross entropy like loss that favors hard examples
@@ -115,8 +161,7 @@ def focal_loss_softmax(logits, labels,  gamma=3., weight_class_sample_prob=False
       train_class_weights_factor=tf.reshape(train_class_weights_factor, [-1,1,y_pred.shape[-1]])
       L*=train_class_weights_factor
     per_sample_loss=tf.reduce_sum(L, axis=-1)
-    L=tf.math.reduce_mean(per_sample_loss, name=name)
-    return L
+    return tf.math.reduce_mean(per_sample_loss, name=name)
 
 def multiclass_dice_loss_softmax(logits, labels, weight_class_sample_prob=False, weight_class_global_prob=False, train_class_probs=None, name='loss'):
     """ multiclass Sørensen-Dice index measure, softmax is applied internally on the y_preds
@@ -151,7 +196,7 @@ def multiclass_jaccard_loss_softmax(logits, labels, weight_class_sample_prob=Fal
           weight_class_sample_prob, set True to weight the loss with respect to sample true class probabilities
           weight_class_global_prob, set True to weight the loss with respect to train dataset true class probabilities
           train_class_probs, a numpy array of class weights
-      Returns the average jaccard loss
+      Returns the average jaccard measures of shape [batchsize, classes]
     """
     y_true, y_pred=preds_labels_preprocess_softmax_flatten(logits, labels)
 
@@ -214,6 +259,14 @@ def multiclass_tversky_loss_softmax(logits, labels, alpha=0.7, weight_class_samp
     #tf.print('tversky is finite', tf.math.reduce_prod(tf.cast(tf.math.is_finite(tversky_losses), dtype=tf.int8)))
     #tf.print('final_loss', tf.reduce_mean(tversky_losses, name=name))
     return tf.reduce_mean(tversky_losses, name=name)
+
+def exponentialLogLoss(loss, gamma=0.3, name='loss'):
+  ''' REMINDER from https://arxiv.org/pdf/1809.00076.pdf
+    apply a power low on the input loss function
+    Args: a tensor of (preliminary weighted) metrics in range [0,1]
+    Returns the average of (-log(loss))^gamma
+  '''
+  return tf.reduce_mean(tf.math.pow(-1.*tf.math.log(loss), gamma), name=name)
 
 # a loss gradient lipshitz regularizer
 def get_IOgradient_norm_lipschitzPenalty(inputs, outputs, target_lipschitz):

helpers/model.py
@@ -81,8 +81,8 @@ class SubpixelConv2D(tf.Module):
     self.lr_conv_channels=self.input_dims[3]*(self.factor**2)
 
     kernel_regul=helpers.loss.Regularizer_L1L2Ortho( l1=0.0,
-                                                     l2=0.0001,
-                                                     ortho=0.,#0001,
+                                                     l2=0.0,
+                                                     ortho=0.001,
                                                      ortho_type='srip',
                                                      nb_filters=self.lr_conv_channels)
       
@@ -134,7 +134,7 @@ def atrous_Spatial_pyramid_pooling(input_features, outing_nb_features=256, rates
     ##1x1 conv
     pooled_features_regul=helpers.loss.Regularizer_L1L2Ortho( l1=0.0,
                                                 l2=0.0,#001,
-                                                ortho=0.0001,
+                                                ortho=0.001,
                                                 ortho_type='srip',
                                                 nb_filters=outing_nb_features)
     pooled_features=tf.keras.layers.Conv2D(
@@ -145,9 +145,9 @@ def atrous_Spatial_pyramid_pooling(input_features, outing_nb_features=256, rates
                           data_format='channels_last',
                           dilation_rate=(1, 1),
                           activation=None,
-                          use_bias=True,
+                          use_bias=False,
                           kernel_initializer=tf.keras.initializers.Orthogonal(),
-                          bias_initializer=tf.initializers.constant(0.001),
+                          bias_initializer=None,#tf.initializers.constant(0.001),
                           kernel_regularizer=pooled_features_regul,
                           bias_regularizer=None,
                           activity_regularizer=None,
@@ -165,7 +165,7 @@ def atrous_Spatial_pyramid_pooling(input_features, outing_nb_features=256, rates
         print('rate, kernel',(rate, kernel_size))
         kernel_regul=helpers.loss.Regularizer_L1L2Ortho( l1=0.0,
                                                 l2=0.0,#001,
-                                                ortho=0.0001,
+                                                ortho=0.001,
                                                 ortho_type='srip',
                                                 nb_filters=outing_nb_features)
         if kernel_size==1:#prefer standard conv
@@ -177,9 +177,9 @@ def atrous_Spatial_pyramid_pooling(input_features, outing_nb_features=256, rates
                           data_format='channels_last',
                           dilation_rate=(1, 1),
                           activation=None,
-                          use_bias=True,
+                          use_bias=False,
                           kernel_initializer=tf.keras.initializers.Orthogonal(),
-                          bias_initializer=tf.initializers.constant(0.001),
+                          bias_initializer=None,#tf.initializers.constant(0.001),
                           kernel_regularizer=kernel_regul,
                           bias_regularizer=None,
                           activity_regularizer=None,
@@ -215,7 +215,7 @@ def atrous_Spatial_pyramid_pooling(input_features, outing_nb_features=256, rates
     #fusion and compression layer
     kernel_regul_asppout=helpers.loss.Regularizer_L1L2Ortho( l1=0.0,
                                                 l2=0.0,
-                                                ortho=0.0001,
+                                                ortho=0.001,
                                                 ortho_type='srip',
                                                 nb_filters=outing_nb_features)
     aspp_features=tf.keras.layers.Conv2D(