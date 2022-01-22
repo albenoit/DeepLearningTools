c30afe7c9f4766fc3ef38f3126f7b8818c323de8
alben <alexandre.benoit@univ-smb.fr>
2022-01-22 22:13:08 +0100

    added kafka interface

---
+++ helpers/kafka_io.py
@@ -0,0 +1,203 @@
+# a set of helpers to experiment with kafka pubsub
+# maes use of tensor serialization/parsing helpers proposed in tensor_msg_io.py
+
+'''
+# tested with kafka install and config:
+wget  https://downloads.apache.org/kafka/2.7.1/kafka_2.13-2.7.1.tgz
+tar -xzf kafka_2.13-2.7.1.tgz
+
+#-> start zookeeper and kafka
+./kafka_2.13-2.7.1/bin/zookeeper-server-start.sh -daemon ./kafka_2.13-2.7.1/config/zookeeper.properties
+./kafka_2.13-2.7.1/bin/kafka-server-start.sh -daemon ./kafka_2.13-2.7.1/config/server.properties
+echo "Waiting for 10 secs until kafka and zookeeper services are up and running"
+sleep 10
+#-> create the 'demo-pics' topic
+./kafka_2.13-2.7.1/bin/kafka-topics.sh --create --bootstrap-server 127.0.0.1:9092 --replication-factor 1 --partitions 1 --topic demo-pics
+#-> check topic behaviors
+./kafka_2.13-2.7.1/bin/kafka-topics.sh --describe --bootstrap-server 127.0.0.1:9092 --topic demo-pics
+
+#-> not forget to delete topic to keep disk space...
+./kafka_2.13-2.7.1/bin/kafka-topics.sh  --bootstrap-server 127.0.0.1:9092 --delete --topic demo-pics
+
+#-> list all available topics on a server:
+./kafka_2.13-2.7.1/bin/kafka-topics.sh  --list --bootstrap-server 127.0.0.1:9092
+
+#-> get topic details:
+./kafka_2.13-2.7.1/bin/kafka-topics.sh  --bootstrap-server=localhost:9092 --describe --topic demo-pics
+'''
+import datetime
+import kafka
+try:
+  import helpers.tensor_msg_io as tensor_msg_io
+except:
+  import tensor_msg_io
+
+import tensorflow as tf
+import tensorflow_io as tfio
+
+def error_callback(exc):
+    raise Exception('Error while sendig data to kafka: {0}'.format(str(exc)))
+
+# tensorflow message publish
+
+class KafkaIO(object):
+  def __init__(self, topic_name:str, bootstrap_servers=['localhost:9092'], flush_every=20, element_spec=None):
+    '''
+      setup a kafka connexion to push Tensorflow data samples to
+      topic_name:str, the log queue name to write to
+      bootstrap_servers, the list of kafka servers(default is default local)
+      flush_every, an integer that specify the iteration period when data is flushed to kafka 
+    '''
+    self.bootstrap_servers=bootstrap_servers
+    self.topic_name=topic_name
+    self.flush_every=flush_every
+    self.element_spec=element_spec
+    print('---> settingup connector to a kafka dataset (servers, queue)', (self.bootstrap_servers, topic_name))
+
+    
+  def kafka_producer_tf(self, items:tf.Tensor, log:str=None)->None:
+    '''
+    Publish tf.tensor sample pairs (tensor, label) in the form of tensorflow.Examples from an iterable (list of tensor tuples, or a dataset (WARNING, each batch will then be a sample) or something similar)
+    '''
+    producer=kafka.KafkaProducer(bootstrap_servers=self.bootstrap_servers)
+
+    count=0
+    
+    for value, key in items:
+      #-> first serialize key (say expected label/ground truth) and value (say input tensor)
+      #print('value', value, type(value))
+      #print('key', key, type(key))
+      #tf.print('value', value)
+      #tf.print('key', key)
+      key_msg=tensor_msg_io.serialize_tensor(key)
+      value_msg=tensor_msg_io.serialize_tensor(value)
+      #print('key_msg', key_msg)
+      #print('value_msg', value_msg)
+      if not(count%self.flush_every):
+        #producer.flush()
+        print('added batchs:',count)
+        #print('value_msg', value_msg)
+        #print('value_msg', type(value_msg))
+      #input(image_msg)
+      producer.send(self.topic_name, key=key_msg, value=value_msg)
+      count+=1
+    """
+    from multiprocessing import Pool
+    print("Number of cpu : ", multiprocessing.cpu_count())
+    p = Pool(multiprocessing.cpu_count()//2)
+    
+    def send_one_example(value, key):
+      key_msg=tensor_msg_io.serialize_tensor(key)
+      value_msg=tensor_msg_io.serialize_tensor(value)
+      producer.send(self.topic_name, key=key_msg, value=value_msg)
+
+    p.map(send_one_example, items)
+    """
+    #finalize
+    metrics = producer.metrics()
+    print('Producer metrics:', metrics)
+    producer.flush()
+
+    msg="\n{date} {top}, {cnt} messages written.".format(date=datetime.datetime.now().strftime("%Y-%m-%d--%H:%M:%S"),
+                                                        top=self.topic_name,
+                                                        cnt=count)
+    print(msg)
+    if log is not None:
+      with open(log, 'a') as f:
+        f.write(msg)
+
+  def kafka_dataset_consumer_tf_basic(self, batch_size:int, shuffle:bool=False)->tfio.IODataset:
+    def decode_kafka_item(item):
+      '''
+      specify here how to decode each item. Their basic structure being to be a pair item.key, item.message, each of them being simple tensors
+      Here the type of each pair element should be taken into account carefully with respect to what was done at the encoding step
+      '''
+      tf.print('message', item.message)
+      tensor = tensor_msg_io.decode_tensor_proto(item.message, dtype=tf.uint8)
+      label =  tensor_msg_io.decode_tensor_proto(item.key, dtype=tf.string)
+      tf.print('decoded tensor, label', tf.shape(tensor), tf.shape(label))
+      return (tensor, label)
+
+    ds = tfio.IODataset.from_kafka(self.topic_name, partition=0, servers=self.bootstrap_servers[0], offset=0)
+    if shuffle is True:
+      shuffle_buffer_size=10*batch_size #default setup for minimal shuffling capability on the client side
+      ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+    ds = ds.map(decode_kafka_item)
+    ds = ds.batch(batch_size).prefetch(1)
+    return ds
+
+  def kafka_dataset_consumer_tf_custom(self, features, batch_size:int, shuffle:bool=False)->tfio.IODataset:
+    def decode_kafka_custom_items(item):
+      '''
+      specify here how to decode each item. Their structure being to be a pair item.key, item.message BUT each can be a set of tensors
+      Here the type of each pair element should be taken into account carefully with respect to what was done at the encoding step
+      '''
+      tensor = tensor_msg_io.decode_multitensor_proto(features[0], item.message)
+      label =  tensor_msg_io.decode_multitensor_proto(features[1], item.key)
+      #tf.print('!!!! decoded', tensor, label)#tensor=label
+      if self.element_spec is not None:
+        for id, col in enumerate([tensor, label]): #applying same stuff for bith data and label columns
+          for key in self.element_spec[id].keys():
+            #print('##################################################\n[-1]+list(self.element_spec[id][key].shape[1:])=',[-1]+list(self.element_spec[id][key].shape[1:]))
+            #tf.print('TO BE RESHAPED'+key, tf.shape(col[key]))
+            col[key]=tf.reshape(col[key], self.element_spec[id][key].shape[1:], name='sample_'+key)
+            #tf.print('RESHAPED'+key+'!!!', tf.shape(col[key]))
+      #tf.print('!!!! RESHAPED', tensor, label)#tensor=label
+      
+      return (tensor, label)
+
+    ds = tfio.IODataset.from_kafka(self.topic_name,
+                                   partition=0,
+                                   servers=self.bootstrap_servers[0],
+                                   offset=0,
+                                   configuration=["conf.topic.auto.offset.reset=earliest"])
+    ds = ds.map(decode_kafka_custom_items, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    if shuffle is True:
+      shuffle_buffer_size=100*batch_size #default setup for minimal shuffling capability on the client side
+      ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+    ds = ds.batch(batch_size).prefetch(batch_size)
+    return ds
+
+  def kafka_dataset_incremental_consumer_tf(self, batch_size:int, shuffle:bool=False)->tfio.IODataset:
+    raise NotImplementedError('Streaming datasets not already implemented')
+    online_train_ds = tfio.experimental.streaming.KafkaBatchIODataset(self.topic_name,
+                                          partition=0,
+                                          servers=self.bootstrap_servers[0],
+                                          stream_timeout=10000, # in milliseconds, to block indefinitely, set it to -1.
+                                          configuration=[
+                                              "session.timeout.ms=7000",
+                                              "max.poll.interval.ms=8000",
+                                              "auto.offset.reset=earliest"
+                                          ])
+    # online_train_ds generates tiny datasets, see https://colab.research.google.com/github/tensorflow/io/blob/master/docs/tutorials/kafka.ipynb#scrollTo=9cxF0bgGkQJs
+    # -> then consider incremental learning on a stream of datasets
+     
+#test code
+if __name__ == "__main__":
+	import cv2
+	# tensorflow kafka demo
+	#-> get demo images
+	kafka_topic='demo-pics'
+	from sklearn.datasets import load_sample_images
+	dataset = load_sample_images()     
+	#print('demo images', dataset.images)
+	images_tf=tf.constant(dataset.images, dtype=tf.uint8)
+	images_labels=tf.constant(dataset.filenames, dtype=tf.string)
+	print('images_tf', images_tf)
+	demo_dataset = zip(images_tf, images_labels) # -> keep the 'key,', 'value' order to comply with the API
+	#-> publish dataset to kafka
+	kafka_writer=KafkaIO(topic_name='demo_pics3')
+	kafka_writer.kafka_producer_tf(demo_dataset)
+	#->read and display dataset samples
+	kafka_reader=KafkaIO(topic_name='demo_pics3')
+	dataset_kclient=kafka_reader.kafka_dataset_consumer_tf_basic(batch_size=1)
+	
+	for id, sample in enumerate(dataset_kclient):
+	  print('sample ', id, ': value, key shapes=', sample[0].shape, sample[1].shape)
+	  cv2.imshow(str(sample[1][0].numpy()), cv2.cvtColor(sample[0][0].numpy(), cv2.COLOR_RGB2BGR))
+
+	cv2.waitKey()
+
+
+
+
---
+++ helpers/tensor_msg_io.py
@@ -0,0 +1,186 @@
+# a set of helpers to convert tensors to their serialized binary strings to facilitate message io accross processes
+# from https://www.tensorflow.org/tutorials/load_data/tfrecord?hl=en
+import tensorflow as tf
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _float_list_feature(values):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature_scalar(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _int64_list_feature(values):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
+
+def _tensor_feature(tensor):
+  #first serialize the tensor
+  serialized_nonscalar = tf.io.serialize_tensor(tensor)
+  #next integrate into a tf.train.Feature
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[serialized_nonscalar.numpy()]))
+
+# Create a dictionary with features that may be relevant.
+def serialize_image_float32_example(image_tensor, label_id=None):
+  
+  feature = {
+      'image_raw': _tensor_feature(image_tensor)#simpler and keeps dimensions info compared to _float_list_feature(image_tensor.numpy().flatten().tolist()),
+  }
+  if label_id is not None:
+    feature.update({'label_id': _int64_feature_scalar(label_id)})
+
+  return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
+
+def serialize_tensor_label_example(tensor, label):
+  
+  feature = {
+      'value': _tensor_feature(tensor),
+      'label': _tensor_feature(label),
+  }
+
+  return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
+
+def serialize_float_with_text_label(values, label):
+  """
+  Creates a tf.train.Example string message ready to be written to a file or sent as a message.
+  """
+  # Create a dictionary mapping the feature name to the tf.train.Example-compatible
+  # data type.
+  feature = {
+      'values': _float_feature(values),
+      'label': _bytes_feature(label),
+  }
+
+  return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
+
+###### Simple tensor + string label encode/decode helpers
+def serialize_tensor_with_label(tensor, label):
+
+  feature = {
+      'values': _tensor_feature(tensor),
+      'label': _bytes_feature(label),
+  }
+  return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
+   
+def decode_tensor_with_label_example(example_proto):
+  ''' decode a tf.train.Example, supposing its structure is known '''
+  # Create a dictionary describing the expected features.
+  data_feature_description = {
+      'values': tf.io.FixedLenFeature([], tf.string),
+      'label': tf.io.FixedLenFeature([], tf.string),
+  }
+
+  features_dict = tf.io.parse_single_example(example_proto, data_feature_description)
+  print('features_dict', features_dict)
+  instance_label=features_dict['label']
+  instance_tensor=tf.io.parse_tensor(features_dict['values'], tf.float32)
+
+  return instance_label, instance_tensor
+
+###### Simple tensor encode/decode helpers
+def serialize_tensor(tensor):
+  ''' simply serialize a single tensor as a tf.Example '''
+  if isinstance(tensor, (list,tuple)):
+    #tf.print('*** input tensor is list or tuple')
+    feature={'values_'+str(keyID):_tensor_feature(item) for keyID, item in enumerate(tensor)}
+  elif isinstance(tensor, dict):
+    #tf.print('*** input tensor is dict')
+    feature={item:_tensor_feature(tensor[item]) for item in tensor.keys()}
+  else: #if tensor is a classical tensor, then, encode with defauult name 'values'
+    #tf.print('*** direct tensor encoding')
+    feature = {
+      'values': _tensor_feature(tensor),
+    }
+
+  return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
+
+def decode_tensor_proto(example_proto, dtype):
+  ''' decode a tf.train.Example, supposing its structure is a simple tensor '''
+  # Create a dictionary describing the expected features.
+  data_feature_description = {
+      'values': tf.io.FixedLenFeature([], tf.string),
+  }
+
+  features_dict = tf.io.parse_single_example(example_proto, data_feature_description)
+  #print('features_dict', features_dict)
+  instance_tensor=tf.io.parse_tensor(features_dict['values'], dtype, name='parse_values')
+
+  return instance_tensor
+
+
+def decode_multitensor_proto(data_feature_descriptions, example_proto):
+  ''' decode a tf.train.Example, supposing its structure is known '''
+  #print('+++++++++++++data_feature_descriptions', data_feature_descriptions['feature_specs'])
+  features_dict = tf.io.parse_single_example(example_proto, data_feature_descriptions['feature_specs'])
+  #print('features_dict', features_dict)
+  instance_tensor={key_name: tf.io.parse_tensor(features_dict[key_name], data_feature_descriptions['types'][key_name], name='parse_'+key_name) for key_name in data_feature_descriptions['types'].keys()}
+  '''for i in instance_tensor.keys():
+    tf.print(i, tf.shape(instance_tensor[i]))
+  '''
+  return instance_tensor
+
+def get_label_data_features_from_dataset(dataset):
+  '''
+  construct a dictionnary of feature description to decode serialized tensors
+  '''  
+  def get_features_specs(dataset_col_specs):
+    data_feature_descriptions={}
+    feature_types={}
+    for keyID in dataset_col_specs.keys():
+      print('item', keyID, dataset_col_specs[keyID])
+      #data_feature_description
+      #name:tf.io.FixedLenFeature([], tf.string)
+
+      # Create a dictionary describing the expected features.
+      data_feature_descriptions[keyID] = tf.io.FixedLenFeature([], tf.string)
+      feature_types[keyID]=dataset_col_specs[keyID].dtype
+
+    return {'feature_specs':data_feature_descriptions, 'types':feature_types}
+  dataset_label_data_element_spec=dataset.element_spec
+  label_specs=get_features_specs(dataset_label_data_element_spec[0])
+  data_specs=get_features_specs(dataset_label_data_element_spec[1])
+  print('label_specs=', label_specs)
+  print('data_specs=', data_specs)
+  return label_specs, data_specs
+  
+
+
+
+############### EXAMPLES ###################
+#encoding example 1 (float scalar, label)
+if __name__ == "__main__":
+  serialized_example_1 = serialize_float_with_text_label(0.1, b'goat')
+  print('serialized example_1', serialized_example_1)
+  #related decoding example
+  read_proto_1 = tf.train.Example.FromString(serialized_example_1)
+  print('unserialized example_1', read_proto_1)
+
+  #encoding example 2 (tensor, label)
+  
+  serialized_example_2 = serialize_tensor_with_label(tf.constant([0.1, 2.03]), b'goat')
+  print('serialized example_2', serialized_example_2)
+  #related decoding example
+  example_proto=tf.constant(serialized_example_2)
+  #example = tf.train.Example()
+  #example.ParseFromString(serialized_example_2)
+  print('unserialized example_2', example_proto)
+  decoded_example=decode_tensor_with_label_example(example_proto)
+  print('unserialized example_2', decoded_example)
+  
+
