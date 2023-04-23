7c6af887a3ad9760687064b43162729eaa3a4ea2
alben <alexandre.benoit@univ-smb.fr>
2023-04-23 18:03:35 +0200

    updated to comply with the new package structure

--- test_framework.py
+++ test_framework.py
@@ -3,15 +3,21 @@ TODO:move to unit testing with pytest
 '''
 
 import pytest
-from tools.command_line_parser import get_default_args
+import tensorflow as tf
+from deeplearningtools.tools.command_line_parser import get_default_args
+from deeplearningtools import experiments_manager
+from deeplearningtools import start_kafka_producer
+from deeplearningtools import start_federated_server
+from deeplearningtools.helpers import tensor_msg_io  
+import os
 
 #basic function that just starts a demo script
-def start_script(FLAGS, experiment_settings_file, experiment_settings_hparams):
+def start_training_script(FLAGS, experiment_settings_file, experiment_settings_hparams):
   global jobState
   global jobSessionFolder
   global loss
-
-  import experiments_manager
+  print('************ CWD=', os.getcwd())
+  
   job_result = experiments_manager.run(FLAGS, train_config_script=experiment_settings_file, external_hparams=experiment_settings_hparams)
   print('trial ended successfully, output='+str(job_result))
   if 'loss' in job_result.keys():
@@ -20,50 +26,72 @@ def start_script(FLAGS, experiment_settings_file, experiment_settings_hparams):
   jobState=True
   return jobState, jobSessionFolder, loss
 
+def test_kafka_producer_no_broker():
+  import kafka
+  with pytest.raises(kafka.errors.NoBrokersAvailable) as ce:
+    FLAGS = start_kafka_producer.get_commands().parse_args([])
+    FLAGS.usersettings='examples/regression/mysettings_curve_fitting.py'
+    print(FLAGS)
+    start_kafka_producer.run(FLAGS)
+
+def test_tensor_msg_io():
+  input_constant=tf.constant([0.1, 2.03], dtype=tf.float16)
+  input_label=b'goat'
+  #encode
+  serialized_example_2 = tensor_msg_io.serialize_tensor_with_label(input_constant, input_label)
+  #decode
+  example_proto=tf.constant(serialized_example_2)
+  decoded_example=tensor_msg_io.decode_tensor_with_label_example(example_proto, tf.float16)
+  print('unserialized example_2', decoded_example)
+  assert   (decoded_example[0]==input_label).numpy()==True and (decoded_example[1]==input_constant).numpy()[0]==True
+
 def test_basic_regression_noHparams():
-  #with pytest.raises(AttributeError) as ce:
   FLAGS = get_default_args()
   FLAGS.usersettings='examples/regression/mysettings_curve_fitting.py'
   print(FLAGS)
-  jobState, jobSessionFolder, loss = start_script(FLAGS, FLAGS.usersettings, {})
+  jobState, jobSessionFolder, loss = start_training_script(FLAGS, FLAGS.usersettings, {})
   print('Test end, loss=', loss)
   assert loss < 10
 
 def test_basic_regression_epochHparams():
-  #with pytest.raises(AttributeError) as ce:
   FLAGS = get_default_args()
   FLAGS.usersettings='examples/regression/mysettings_curve_fitting.py'
   print(FLAGS)
-  jobState, jobSessionFolder, loss = start_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
+  jobState, jobSessionFolder, loss = start_training_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
   print('Test end, loss=', loss)
   assert loss < 50
 
 def test_timeseries():
-  #with pytest.raises(AttributeError) as ce:
   FLAGS = get_default_args()
   FLAGS.usersettings='examples/timeseries/mysettings_timeseries_forecasting.py'
   print(FLAGS)
-  jobState, jobSessionFolder, loss = start_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
+  jobState, jobSessionFolder, loss = start_training_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
   print('Test end, loss=', loss)
   assert loss < 500
 
 def test_classification():
-  #with pytest.raises(AttributeError) as ce:
   FLAGS = get_default_args()
   FLAGS.usersettings='examples/classification/mysettings_image_classification.py'
   print(FLAGS)
-  jobState, jobSessionFolder, loss = start_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
+  jobState, jobSessionFolder, loss = start_training_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
   print('Test end, loss=', loss)
   assert loss < 2
 
 def test_basic_1Dembedding():
-  #with pytest.raises(AttributeError) as ce:
   FLAGS = get_default_args()
   FLAGS.usersettings='examples/embedding/mysettings_1D_experiments.py'
   print(FLAGS)
-  jobState, jobSessionFolder, loss = start_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
+  jobState, jobSessionFolder, loss = start_training_script(FLAGS, FLAGS.usersettings, {'nbEpoch':2})
   print('Test end, loss=', loss)
   assert loss < 500
+
+def test_flower_simulation():
+  
+  FLAGS = start_federated_server.get_commands().parse_args([])
+  FLAGS.usersettings='examples/federated/mysettings_curve_fitting.py'
+  FLAGS.simulation=True
+  start_federated_server.run(FLAGS)
+  assert True #FIXME: check returned parameter value
 
 if __name__ == "__main__":
   print('Starting some test functions as demos, please consider running pytest like this :')
