4cafd673d72cfad0b80f73c0c536d5a6627bf912
alben <alexandre.benoit@univ-smb.fr>
2024-03-04 08:37:49 +0100

    improved

--- test_framework.py
+++ test_framework.py
@@ -12,10 +12,12 @@
 import pytest
 import tensorflow as tf
 from deeplearningtools.tools.command_line_parser import get_default_args
+from deeplearningtools.tools.experiment_settings import  loadExperimentsSettings, loadModel_def_file, SETTINGSFILE_COPY_NAME
 from deeplearningtools import experiments_manager
 from deeplearningtools import start_kafka_producer
 from deeplearningtools import start_federated_server
 from deeplearningtools.helpers import tensor_msg_io  
+from deeplearningtools.tools import experiments_settings_surgery
 import os
 
 #basic function that just starts a demo script
@@ -93,20 +95,23 @@ def test_basic_1Dembedding():
   assert loss < 500
 
 def test_flower_simulation():
-  
+  expected_nb_rounds=1
   FLAGS = start_federated_server.get_commands().parse_args([])
   FLAGS.usersettings='examples/federated/mysettings_curve_fitting.py'
   FLAGS.simulation=True
-  start_federated_server.run(FLAGS)
-  assert True #FIXME: check returned parameter value
+  FLAGS.num_rounds=expected_nb_rounds
+  # run federated experiment in simulation mode
+  result=start_federated_server.run(FLAGS)#, {'session_number':0, 'session_folder':sessionFolder})
+  assert len(result.metrics_centralized['loss']) == expected_nb_rounds+1
 
 if __name__ == "__main__":
   print('Starting some test functions as demos, please consider running pytest like this :')
   print(' pytest test_framework.py OR FROM A CONTAINER, apptainer exec path/to/tf2_addons.sif pytest test_framework.py')
   # manually choose a given test
+  test_flower_simulation()
+  test_basic_1Dembedding()
   test_basic_regression_noHparams()
   test_basic_regression_epochHparams()
   test_timeseries()
   test_classification()
-  test_basic_1Dembedding()
   print("END")
