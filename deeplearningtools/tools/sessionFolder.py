845b3a460c12fd5f0cfb058f04f3c48847d78578
alben <alexandre.benoit@univ-smb.fr>
2024-04-12 16:07:24 +0200

    fix too log experiment folder name by adding truncation

--- deeplearningtools/tools/sessionFolder.py
+++ deeplearningtools/tools/sessionFolder.py
@@ -1,4 +1,4 @@
-
+import os
 
 def create_session_folder(sessionFolderBase, usersettings):
     sessionFolder = sessionFolderBase
@@ -7,5 +7,10 @@ def create_session_folder(sessionFolderBase, usersettings):
         sessionFolder_addon+='_'+key+str(value)
     #insert sessionname addons in the original one
     sessionFolder+=sessionFolder_addon
+
+    #truncate if path exceeds sytem max length
+    max_length = os.pathconf('/', 'PC_NAME_MAX')
+    if len(sessionFolder)>max_length:
+        sessionFolder=sessionFolder[:max_length]
     #print('Found hparams: '+str(usersettings.hparams))
     return sessionFolder
